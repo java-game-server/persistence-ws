32eea54a037e57970c352aae769e004950328b60
Raikish <albert.montagut.casero@gmail.com>
2018-03-23 13:32:21 +0100

    configuring spring project

---
+++ src/main/java/com/apporelbotna/gameserver/persistencews/Application.java
@@ -0,0 +1,12 @@
+package com.apporelbotna.gameserver.persistencews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
