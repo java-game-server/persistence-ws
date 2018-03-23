cf361927b3a637fe93d736639353b34445b193db
Raikish <albert.montagut.casero@gmail.com>
2018-03-23 14:19:48 +0100

    adding JDBC connector to postgreSQL + testing connection

--- src/main/java/com/apporelbotna/gameserver/persistencews/PostgreDAO.java
+++ src/main/java/com/apporelbotna/gameserver/persistencews/PostgreDAO.java
@@ -1,6 +1,28 @@
 package com.apporelbotna.gameserver.persistencews;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class PostgreDAO
 {
+	private final String url = "jdbc:postgresql://localhost/AppOrElBotnaGameClient";
+	private final String user = "root";
+	private final String password = "root";
 
+	 public PostgreDAO() {
+		 
+	 }
+	 
+	 public Connection connect() {
+	        Connection conn = null;
+	        try {
+	            conn = DriverManager.getConnection(url, user, password);
+	            System.out.println("Connected to the PostgreSQL server successfully.");
+	        } catch (SQLException e) {
+	            System.out.println(e.getMessage());
+	        }
+	 
+	        return conn;
+	    }
 }
