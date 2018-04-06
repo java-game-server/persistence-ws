90f1d793a99a73e316b684da19e888caef47d84b
Jendoliver <alvarezmonllor@gmail.com>
2018-04-06 04:04:30 +0200

    Fixing ApplicationProperties

---
+++ src/main/java/com/apporelbotna/gameserver/persistencews/properties/ApplicationProperties.java
@@ -0,0 +1,42 @@
+package com.apporelbotna.gameserver.persistencews.properties;
+
+import java.util.ResourceBundle;
+
+public final class ApplicationProperties
+{
+	private static String version;
+	private static String name;
+
+	private static String serverUrl;
+
+	private ApplicationProperties()
+	{
+		throw new UnsupportedOperationException("ApplicationProperties must not be instantiated!");
+	}
+
+	static
+	{
+		ResourceBundle bundle = ResourceBundle
+				.getBundle("com.apporelbotna.gameserver.persistencews.properties.application");
+
+		version = bundle.getString("version");
+		name = bundle.getString("name");
+
+		serverUrl = bundle.getString("server.url");
+	}
+
+	public static String getVersion()
+	{
+		return version;
+	}
+
+	public static String getName()
+	{
+		return name;
+	}
+
+	public static String getServerUrl()
+	{
+		return serverUrl;
+	}
+}
